716bc9e964ecdbd8f327aaacc0c8e0d01bb73bcb
imtiazhasanBD <imtiazhasan500@gmail.com>
2024-06-11 02:56:00 +0600

    creating Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {CiSearch,CiShoppingCart} from 'react-icons/ci'
 
 const Main = () => {
   return (
     <div className='ml-[80px]'>
-      Main page
+      <header className='flex justify-between items-center px-5 py-2'>
+        <div className="text-3xl font-bold text-blue-400 ">Electro-mart</div>
+        <div className='flex justify-between items-center px-5 py-2 bg-gray-100 rounded'>
+            <input type="text" placeholder='Search product' className='bg-transparent outline-0'/>
+            <button className='text-blue-400' onClick={()=> searchHandler()}><CiSearch/></button>
+        </div>
+      </header>
     </div>
   )
 }
