a4b6ab327661b79b93296457ea7de6122fcee0b8
imtiazhasanBD <imtiazhasan500@gmail.com>
2024-09-03 18:24:05 +0600

    adding product filter functality

--- src/components/ProductSearch.jsx
+++ src/components/ProductSearch.jsx
@@ -12,20 +12,26 @@ const ProductSearch = () => {
   const [filterOpen, setFilterOpen] = useState(false);
   const { title } = useParams();
   const [filterValue, setFilterValue] = useState('');
-  const [filterProducts, setFilterProducts] = useState([state.searchProducts[0]])
+  const [filterProducts, setFilterProducts] = useState(state.searchProducts[0]);
   const [activeSort, setActiveSort] = useState("Best Match");
-  
 
-    // Create unique filters for categories and brands
-    const filters = {
-      categorys: [...new Set(state.searchProducts[0].map((product) => product.category))],
-      brands: [...new Set(state.searchProducts[0].map((product) => product.brand || "No Brand"))],
-    };
+
+  // Create unique filters for categories and brands
+  const filters = {
+    categorys: [
+      ...new Set(state.searchProducts[0]?.map((product) => product.category)),
+    ],
+    brands: [
+      ...new Set(
+        state.searchProducts[0]?.map((product) => product.brand || "No Brand")
+      ),
+    ],
+  };
 
   const handleSortClick = (sortOption) => {
     setActiveSort(sortOption);
 
-    let sortedProducts = [...state.searchProducts[0]];
+    let sortedProducts = [...filterProducts];
 
     switch (sortOption) {
       case "Low To High":
@@ -46,18 +52,32 @@ const ProductSearch = () => {
         break;
     }
 
-    setFilterProducts([sortedProducts])
+    setFilterProducts(sortedProducts);
   };
 
   useEffect(() => {
-    setFilterProducts([...state.searchProducts]); // Ensure products are reset when state changes
+    setFilterProducts(state.searchProducts[0]); // Ensure products are reset when state changes
   }, [state.searchProducts]);
 
   const handleFilterProducts = (value) => {
-    setFilterValue(value)
-    let sortedProducts = [...state.searchProducts[0]].filter(product => product.brand === value || product.category === value);
-     setFilterProducts([sortedProducts])
-  }
+    setFilterValue(value);
+    let sortedProducts = [...state.searchProducts[0]].filter(
+      (product) =>
+        product.brand === value ||
+        product.category === value ||
+        (value === "50" ? product.price <= 50 : false) ||
+        (value === "100"
+          ? product.price <= 100 && product.price > 50
+          : false) ||
+        (value === "200"
+          ? product.price <= 200 && product.price > 100
+          : false) ||
+        (value === "201" ? product.price > 200 : false)
+    );
+    setFilterProducts(sortedProducts);
+    console.log(value);
+  };
+
   const sortOptions = [
     "Best Match",
     "Top Sales",
@@ -72,9 +92,15 @@ const ProductSearch = () => {
         <h1 className="text-2xl font-bold">Filters</h1>
         <div className="flex flex-col">
           <h3 className="text-md font-semibold border-b-2 pb-2">Category</h3>
-          {state.searchProducts[0] &&
+          {filters.categorys &&
             filters.categorys.map((category) => (
-              <span key={uuidv4()} className={`pt-2 ${category === filterValue? 'text-blue-500': ''}`} value={category} onClick={() => handleFilterProducts(category)}>
+              <span
+                key={uuidv4()}
+                className={`pt-2 ${
+                  category === filterValue ? "text-blue-500" : ""
+                }`}
+                onClick={() => handleFilterProducts(category)}
+              >
                 {category}
               </span>
             ))}
@@ -82,10 +108,13 @@ const ProductSearch = () => {
         <div className="product-brand">
           <div className="product-catagory product-brand-list flex flex-col">
             <h3 className="text-md font-semibold border-b-2 pb-2">Brands</h3>
-            {state.searchProducts[0] &&
+            {filters.brands &&
               filters.brands.map((brand) => (
                 <label key={uuidv4()} className="pt-2">
-                  <input type="checkbox" value={brand} onClick={() => handleFilterProducts(brand)} />
+                  <input
+                    type="checkbox"
+                    onClick={() => handleFilterProducts(brand)}
+                  />
                   <span className="pl-2">
                     {typeof brand === "undefined" ? "No Brand" : brand}
                   </span>
@@ -96,23 +125,43 @@ const ProductSearch = () => {
         <div className="product-price flex flex-col">
           <h3 className="text-md font-semibold border-b-2 pb-2">Price $</h3>
           <label className="pt-2">
-            <input type="checkbox" name="option" value="50" onClick={() => handleFilterProducts(event)}/>
+            <input
+              type="checkbox"
+              name="option"
+              value="50"
+              onClick={() => handleFilterProducts("50")}
+            />
             <span className="pl-2">under - 50</span>
           </label>
           <label>
-            <input type="checkbox" name="option" value="100" onClick={() => handleFilterProducts(event)}/>
+            <input
+              type="checkbox"
+              name="option"
+              value="100"
+              onClick={() => handleFilterProducts("100")}
+            />
             <span className="pl-2">50 - 100</span>
           </label>
           <label>
-            <input type="checkbox" name="option" value="200" onClick={() => handleFilterProducts(event)} />
+            <input
+              type="checkbox"
+              name="option"
+              value="200"
+              onClick={() => handleFilterProducts("200")}
+            />
             <span className="pl-2">100 - 200</span>
           </label>
           <label>
-            <input type="checkbox" name="option" value="201" onClick={() => handleFilterProducts(event)} />
+            <input
+              type="checkbox"
+              name="option"
+              value="201"
+              onClick={() => handleFilterProducts("201")}
+            />
             <span className="pl-2">200 - over</span>
           </label>
         </div>
-        <div className="product-color">
+        {/* <div className="product-color">
           <div className="product-catagory flex flex-col">
             <h3 className="text-md font-semibold border-b-2 pb-2">Colors</h3>
             <label className="pt-2">
@@ -132,7 +181,7 @@ const ProductSearch = () => {
               <span>Red</span>
             </label>
           </div>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex-1 bg-white drop-shadow-sm lg:p-4">
@@ -144,7 +193,7 @@ const ProductSearch = () => {
           <span className="text-blue-500"> red</span>
         </div>
         <p className="font-semibold text-lg pb-2 px-4 lg:px-0">
-          Showing {state.searchProducts[0].length} results for "{title}"
+          Showing {filterProducts.length} results for "{title}"
         </p>
         <section className="md:flex gap-5 drop-shadow-sm border-b mb-2 hidden">
           <span className="font-semibold">Sort By:</span>
@@ -207,12 +256,12 @@ const ProductSearch = () => {
               <h3 className="text-md font-semibold border-b-2 pb-2">
                 Category
               </h3>
-              {state.searchProducts[0] &&
+              {filters.categorys &&
                 filters.categorys.map((category) => (
                   <span
                     key={uuidv4()}
                     className="pt-2 cursor-pointer"
-                    onClick={() => handleFilterClick(category)}
+                    onClick={() => handleFilterProducts(category)}
                   >
                     {category}
                   </span>
@@ -221,13 +270,17 @@ const ProductSearch = () => {
             {/* Brand Filter */}
             <div className="flex flex-col mb-4">
               <h3 className="text-md font-semibold border-b-2 pb-2">Brands</h3>
-              {state.searchProducts[0] &&
+              {filters.brands &&
                 filters.brands.map((brand) => (
                   <label
                     key={uuidv4()}
                     className="pt-2 flex items-center cursor-pointer"
                   >
-                    <input type="checkbox" value={brand} />
+                    <input
+                      type="checkbox"
+                      value={brand}
+                      onClick={() => handleFilterProducts(brand)}
+                    />
                     <span className="pl-2">
                       {typeof brand === "undefined" ? "No Brand" : brand}
                     </span>
@@ -238,24 +291,44 @@ const ProductSearch = () => {
             <div className="flex flex-col mb-4">
               <h3 className="text-md font-semibold border-b-2 pb-2">Price $</h3>
               <label className="pt-2 flex items-center cursor-pointer">
-                <input type="checkbox" name="option" value="50" />
+                <input
+                  type="checkbox"
+                  name="option"
+                  value="50"
+                  onClick={() => handleFilterProducts("50")}
+                />
                 <span className="pl-2">under - 50</span>
               </label>
               <label className="flex items-center cursor-pointer">
-                <input type="checkbox" name="option" value="100" />
+                <input
+                  type="checkbox"
+                  name="option"
+                  value="100"
+                  onClick={() => handleFilterProducts("100")}
+                />
                 <span className="pl-2">50 - 100</span>
               </label>
               <label className="flex items-center cursor-pointer">
-                <input type="checkbox" name="option" value="200" />
+                <input
+                  type="checkbox"
+                  name="option"
+                  value="200"
+                  onClick={() => handleFilterProducts("200")}
+                />
                 <span className="pl-2">100 - 200</span>
               </label>
               <label className="flex items-center cursor-pointer">
-                <input type="checkbox" name="option" value="201" />
+                <input
+                  type="checkbox"
+                  name="option"
+                  value="201"
+                  onClick={() => handleFilterProducts("201")}
+                />
                 <span className="pl-2">200 - over</span>
               </label>
             </div>
             {/* Color Filter */}
-            <div className="flex flex-col mb-4">
+          {/*   <div className="flex flex-col mb-4">
               <h3 className="text-md font-semibold border-b-2 pb-2">Colors</h3>
               <label className="pt-2 flex items-center cursor-pointer">
                 <input type="checkbox" value="Black" />
@@ -273,7 +346,7 @@ const ProductSearch = () => {
                 <input type="checkbox" value="Red" />
                 <span className="pl-2">Red</span>
               </label>
-            </div>
+            </div> */}
           </div>
         )}
 
@@ -282,8 +355,8 @@ const ProductSearch = () => {
             sortOpen ? "opacity-50" : ""
           }`}
         >
-          {filterProducts[0] &&
-            filterProducts[0].map((product) => (
+          {filterProducts &&
+            filterProducts.map((product) => (
               <Product product={product} key={uuidv4()} />
             ))}
         </div>
